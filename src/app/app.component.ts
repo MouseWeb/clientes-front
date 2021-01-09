664722c88d18628b2be735fd1e52c20e0c496d9f
Douglas <douglas@mouseweb.com.br>
2021-01-08 23:46:57 -0300

    adicionado Toggle sidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+
+import jQuery from 'jquery'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'front-mouseweb';
+
+  ngAfterViewInit(){
+    (function($) {
+        "use strict";
+    
+        // Adicionar estado ativo aos links de navegação da barra lateral
+        var path = window.location.href; // porque a propriedade 'href' do elemento DOM é o caminho absoluto
+            $("#layoutSidenav_nav .sb-sidenav a.nav-link").each(function() {
+                if (this.href === path) {
+                    $(this).addClass("active");
+                }
+            });
+    
+        // Alternar a navegação lateral
+        $("#sidebarToggle").on("click", function(e) {
+            e.preventDefault();
+            $("body").toggleClass("sb-sidenav-toggled");
+        });
+    })(jQuery);
+  }
 }
